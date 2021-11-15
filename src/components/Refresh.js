9d131e0ce5a982b45dd584c8b4b82cc1f5336cae
Aishwar Malviya (Sindbad) <59655703+Aishwarode17@users.noreply.github.com>
2021-11-15 17:39:20 +0530

    Update Refresh.js

--- src/components/Refresh.js
+++ src/components/Refresh.js
@@ -6,7 +6,7 @@ export default function Refresh({clicked}) {
 
     return (
         <div className="container refr">
-            <button type="button" className="btn btn-outline-dark" onClick={clicked}>refresh</button>
+            <button type="button" className="btn btn-outline-dark" onClick={clicked}>Load More</button>
         </div>
     )
 }
